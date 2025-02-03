e275edb62244f888a9ca89c4fda2aa183ef98ed5
nghS <thnghia84@gmail.com>
2025-02-03 19:19:57 +0100

    🗃️✨: ajout ORM prisma et modules User et Auth

--- exo/J1/hello-world/src/app.module.ts
+++ exo/J1/hello-world/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthControllerController } from './Authentification/auth-controller.controller';
+import { AuthModuleModule } from './Authentification/auth-module.module';
+import { AuthService } from './Authentification/auth-service.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModuleModule],
+  controllers: [AppController, AuthControllerController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
